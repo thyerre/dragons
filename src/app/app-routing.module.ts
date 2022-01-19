d364d749d74f748747dc843b6befd8eb32da039d
thyerre <thyerre321@gmail.com>
2022-01-19 12:32:58 -0300

    iniciando telas auth e home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './screens/auth/auth.component';
+import { HomeComponent } from './screens/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
